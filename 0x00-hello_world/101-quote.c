0f2ab542eb210d4b215d405976030ed12799fa57
pauline <victormlongoti@gmail.com>
2023-03-16 14:17:38 -0700

    101task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,14 @@
 #include <unistd.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, quote, sizeof(quote) - 1);
-    return 1;
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, quote, sizeof(quote) - 1);
+return 1;
 }
 
