8315d6d445f3f5a8cf679bcb5313bea1e09c85ab
pauline <victormlongoti@gmail.com>
2023-03-21 09:15:18 -0700

    task 0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,15 @@
 #include "main.h"
 
 /**
- * main - Prints Holberton as a message.
- *
- * Description: This function prints _putchar using the _putchar function
- * Return: Always 0 (Success)
+ * print_alphabet - print all alphabet in lowercase
  */
-int main(void)
-{
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 	_putchar('\n');
-	return (0);
 }
